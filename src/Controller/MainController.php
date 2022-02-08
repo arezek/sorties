d5ca7b199b056536d8b3cbb9304e3fb8b6068c59
clement_jum <cjumeline@tutanota.com>
2022-02-08 09:15:05 +0100

    Modification formulaire et création index, logo, controller, profil

---
+++ src/Controller/MainController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_index")
+     */
+    public function index(): Response
+    {
+        //todo: envoyer une variable (true/false) pour savoir si on est connecter pour afficher son etat de connect navbar
+        //todo: modifier pour afficher la date du jour, le nom d'un participant
+
+        $options = array('campus1', 'campus2', 'campus3');
+        $valeur = array('Parc', '10/11/2023', '31/12/2024', '3/5', 'En cours', 'X', 'Jacques', 'Afficher');
+        $valeurs = array($valeur, $valeur, $valeur);
+
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+            'dateDuJour' => '09/02/2022',
+            'participantPrenom' => 'Clement',
+            'participantNom' => 'J.',
+            'options' => $options,
+            'valeurs' => $valeurs
+        ]);
+    }
+
+    /**
+     * @Route("/Profil/{id}", name="main_profil")
+     */
+    public function profil(): Response
+    {
+        //todo: afficher le nom du profil en title, si id = le miens un bouton apparait pour modifier le profil.
+        //todo: modifier le chemin de navbar car redirige vers le '2'
+        //todo : problème d'affichage du logo juste sur cette page...
+
+        return $this->render('main/profil/profil.html.twig');
+    }
+
+    /**
+     * @Route("/MonProfil", name="main_editprofil")
+     */
+    public function editProfil(): Response
+    {
+        return $this->render('main/profil/editProfil.html.twig');
+    }
+}
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,37 +17,35 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('prenom')
+            ->add('nom')
             ->add('mail')
-            ->add('agreeTerms', CheckboxType::class, [
+            /*->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
+            ])*/
+            ->add('MotDePasse', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Renseignez votre mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
             ])
-            ->add('prenom')
-            ->add('nom')
             ->add('telephone')
-
-
         ;
     }
 
