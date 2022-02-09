f519cf7d138af7b1fb9eacf532a2de62f26101bb
arezek <GoldWhey22@>
2022-02-09 11:01:56 +0100

    regex isok

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,7 +32,7 @@ class RegistrationController extends AbstractController
             $user->setPassword(
             $userPasswordHasher->hashPassword(
                     $user,
-                    $form->get('MotDePasse')->getData()
+                    $form->get('password')->getData()
                 )
             );
 
