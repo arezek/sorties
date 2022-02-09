67cdc623df60ecd49aec45ec0c049f77a909ba7c
LCB <loic.cecyl@gmail.com>
2022-02-09 16:02:34 +0100

    sql LCB

---
+++ SqlBaseLCB.sql
@@ -0,0 +1,10 @@
+INSERT INTO `ville`(`nom`, `code_postal`) VALUES ('Nantes','44000');
+INSERT INTO `ville`(`nom`, `code_postal`) VALUES ('Rennes','35000');
+INSERT INTO `ville`(`nom`, `code_postal`) VALUES ('Noirt','79000');
+INSERT INTO `ville`(`nom`, `code_postal`) VALUES ('Quimper','29000');
+
+INSERT INTO `etat`(`libelle`) VALUES ('Créée');
+INSERT INTO `etat`(`libelle`) VALUES ('Ouverte');
+INSERT INTO `etat`(`libelle`) VALUES ('Clôturée');
+INSERT INTO `etat`(`libelle`) VALUES ('Activité en cours');
+INSERT INTO `etat`(`libelle`) VALUES ('Annulée');
