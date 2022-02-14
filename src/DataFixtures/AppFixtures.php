1e9b7e8e0a7c71b46b197a3039d743bb88b08e36
LCB <loic.cecyl@gmail.com>
2022-02-14 17:53:05 +0100

    correction fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -66,7 +66,7 @@ class AppFixtures extends Fixture
             $manager->flush();
         }
 
-        // Création de 10 campus dont le premier est par défaut
+        // Création de campus dont le premier est par défaut
         $campuses = ['0', 'Rennes', 'Nantes', 'Quimper', 'Niort'];
         $campus = [];
         $campus[0] = new Campus();
@@ -108,44 +108,6 @@ class AppFixtures extends Fixture
         // Création de 30 Sorties
         $sortie = [];
 
-        //______________________________________________________________________ Admin
-
-        for ($i = 0; $i < $faker->numberBetween($min = 5, $max = count($etat) - 1); $i++) {
-            $sortie[$i] = new Sortie();
-            $sortie[$i]->setNom($faker->sentence($nbWords = 2, $variableNbWords = true));
-            $sortie[$i]->setdateHeureDebut($faker->dateTimeInInterval($startDate = '+ 10 days', $interval = '+20 day', $timezone = null));
-            $sortie[$i]->setDuree($faker->dateTimeInInterval($startDate = 'now', $interval = '+1 day', $timezone = null));
-            $sortie[$i]->setDateLimiteInscription($faker->dateTimeInInterval($startDate = 'now', $interval = '+10 day', $timezone = null));
-            $sortie[$i]->setNbInscriptionsMax($faker->numberBetween($min = 5, $max = 20));
-            $sortie[$i]->setInfosSortie($faker->realText(180));
-
-            switch ($i) {
-                case '0':
-                    $sortie[$i]->setEtat($etat[0]);
-                    break;
-                case '1':
-                    $sortie[$i]->setEtat($etat[1]);
-                    break;
-                case '2':
-                    $sortie[$i]->setEtat($etat[2]);
-                    break;
-                case '3':
-                    $sortie[$i]->setEtat($etat[3]);
-                    break;
-                case '4':
-                    $sortie[$i]->setEtat($etat[4]);
-                    break;
-            }
-            $sortie[$i]->setLieu($lieu[$faker->numberBetween($min = 0, $max = count($lieu) - 1)]);
-            $sortie[$i]->setCampus($campus[$faker->numberBetween($min = 0, $max = count($campus) - 1)]);
-            $sortie[$i]->setOrganisateur($participant[0]);
-            for ($j = 0; $j < $faker->numberBetween($min = 0, $max = $sortie[$i]->getNbInscriptionsMax()); $j++) {
-                $sortie[$i]->addParticipant($participant[$faker->numberBetween($min = 0, $max = count($participant) - 1)]);
-            }
-
-            $manager->persist($sortie[$i]);
-            $manager->flush();
-        }
         //______________________________________________________________________ Hasard
         for ($i = 0; $i < 30; $i++) {
             $sortie[$i] = new Sortie();
@@ -155,9 +117,7 @@ class AppFixtures extends Fixture
             $sortie[$i]->setDateLimiteInscription($faker->dateTimeInInterval($startDate = 'now', $interval = '+10 day', $timezone = null));
             $sortie[$i]->setNbInscriptionsMax($faker->numberBetween($min = 5, $max = 20));
             $sortie[$i]->setInfosSortie($faker->sentence);
-
-            $sortie[$i]->setEtat($etat[0]);
-
+            $sortie[$i]->setEtat($etat[$faker->numberBetween($min = 0, $max = count($etat) - 1)]);
             $sortie[$i]->setLieu($lieu[$faker->numberBetween($min = 0, $max = count($lieu) - 1)]);
             $sortie[$i]->setCampus($campus[$faker->numberBetween($min = 0, $max = count($campus) - 1)]);
             $sortie[$i]->setOrganisateur($participant[$faker->numberBetween($min = 0, $max = count($participant) - 1)]);
