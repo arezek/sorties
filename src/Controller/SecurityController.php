57e4e23b742ff65a4ad74204e5e1ccc9f2645fe4
RobinFerre <zazue@LAPTOP-BDK0CQVE>
2022-02-17 11:44:04 +0100

    création mdp oublie

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Service\ResetInterface;
 
 class SecurityController extends AbstractController
 {
@@ -35,4 +41,40 @@ class SecurityController extends AbstractController
         $this->addFlash('notice', 'Vous ête déconnecté !');
         return $this->render('main/index.html.twig');
     }
-}
+    /**
+     * @Route("/forgottenPassword", name="mdp")
+     */
+    public function forgottenPassword(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('mail', EmailType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $mail = $form->getData('mail');
+            var_dump($mail);
+            $em = $this->getDoctrine()->getManager();
+
+            $participant = $em->getRepository(Participant::class)
+                ->findOneBy([
+                    'mail' => $mail
+                ]);
+            if (!$participant) {
+                $this->addFlash('warning', "Cet email n'existe pas.");
+                return $this->redirectToRoute("app_login");
+
+            } else {
+
+                $this->addFlash('warning', "Un email a été envoyé");
+                return $this->redirectToRoute("main_index");
+            }
+        }
+                    return $this->render('security/forgottenPassword.html.twig', [
+                        'form' => $form->createView()
+                    ]);
+                }
+            }
